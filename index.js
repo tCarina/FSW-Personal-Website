88a7208b87aa0602e0fec57831eb16bc27791569
Cari <t.carina@rocketmail.com>
2021-03-20 13:01:44 -0400

    Started contact page

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ aP.innerText = "Hello! My name is Carina and I am a fullstack web engineer born 
 
 let textP = document.querySelector("#tap")
 
-textP.innerText = "Info goes here"
+textP.innerText = "Escape 120 is an interactive horror text adventure game played in the terminal.\n A mysterious disease has turned everyone around you in to undead canibals.\n You come across a dead soldier and through his walkie talkie you discover a cure is possible. \nWith a bit of guidence from Sergeant Pheonix you must make your way through 3 floors\n finding important items and avoiding being bitten."
 
 let snowP = document.querySelector("#smp")
 
-snowP.innerText = "Info goes here"
+snowP.innerText = "Snowman is a simple word guessing game. A word is randomly chosen from the\n games extensive dictionary and it's up to you to guess the letters.\n You get 10 wrong guesses before you lose and the word is revealed. The game\n lets you know the letters you've guessed so far and how many wrong\n guesses you have left."
