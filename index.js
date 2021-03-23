ef2585821cbc369705888c2327c1b4f3dccdb300
Cari <t.carina@rocketmail.com>
2021-03-22 20:06:04 -0400

    done

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let factB = document.querySelector("#newF")
 
 let factA = ["I have a chinchilla named Nube.", "I am Dominican/Guatemalan American.", "I am a first generation American.", "Baking and cooking are one of my favorite ways to destress.", "I love spicy food!", "I am a foodie and an adventurous eater.", "My hair has been just about every color of the rainbow.", "I love any type of horror or psychological thriller show/movie/anime.", "Stephen King is my favorite author.", "I listen to a very wide range of music, from Metalcore to Melodic Dubstep.", "I can eat Japanese or Korean food for every meal.", "I don't drink alcohol.", "My boyfriend is in a industrial hardcore band called Null. (He's also a FS Dev.)", "I've played over 200 hrs of Animal Crossing: New Horizons"]
 
-factB.addEventListener("click" , e => {
+factB.addEventListener("click" , e => { 
     e.preventDefault()
     factP.textContent = " "
     let ranNum = Math.floor(Math.random() * 14)
